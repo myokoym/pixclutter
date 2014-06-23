ae5436f55709afbec5737145acd3679b9d8ced01
Masafumi Yokoyama <myokoym@gmail.com>
2014-06-23 21:05:57 +0900

    Rearrange sourece files

---
+++ include/pixlutter.h
@@ -0,0 +1,6 @@
+#ifndef __PIXLUTTER_H__
+#define __PIXLUTTER_H__
+
+#include "pixlutter/window.h"
+
+#endif /* __PIXLUTTER_H__ */
--- src/pixlutter.c
+++ src/pixlutter.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "pixlutter/window.h"
+#include "pixlutter.h"
 
 int
 main(int argc, char *argv[])
